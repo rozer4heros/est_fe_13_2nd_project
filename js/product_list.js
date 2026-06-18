fba63f1712773aee979de6b19e7c609437d7ecb3
rozer4heros <rozer4heros@gmail.com>
2026-06-18 13:48:09 +0900

    feat: 기본 상품 카드 출력 구현

--- js/product_list.js
+++ js/product_list.js
@@ -6,11 +6,14 @@
 // DOM Selectors
 // ==========================================
 
+const productListEl = document.querySelector(".product_list");
+
 // ==========================================
 // State & Constants
 // ==========================================
 
 let allProducts = [];
+let filteredProducts = [];
 
 // ==========================================
 // Functions & Core Logic
@@ -19,7 +22,7 @@ let allProducts = [];
 async function fetchProducts() {
   await fetch("../data/products.json")
     .then(response => {
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+      if (!response.ok) throw new Error(`HTTP 에러! 상태 코드: ${response.status}`);
       return response.json();
     })
     .then(result => {
@@ -29,7 +32,47 @@ async function fetchProducts() {
       console.error("데이터 로드 실패: ", error);
     });
 
-  console.log(allProducts);
+  filteredProducts = allProducts.filter(product => !product.isSoldOut);
+
+  renderProducts();
+}
+
+function renderProductCard(product) {
+  const itemEl = document.createElement("li");
+  itemEl.innerHTML = `
+    <article class="product_card">
+      <a href="details.html" class="product_card_imgbox">
+        <img src="${product.mainImage}" alt="" />
+        <button class="try_on_btn body_xl text-center">TRY ON</button>
+      </a>
+      <div class="d-flex flex-column g-0-5">
+        <h3 class="product_name body_xl">
+          <a href="details.html">${product.name}</a>
+        </h3>
+        <div class="product_card_header d-flex justify-content-between align-items-center">
+          <span class="brand display_h3">${product.brand}</span>
+          <button class="like product_card_wish_btn material-symbols-rounded">heart_plus</button>
+        </div>
+        <div class="product_card_price d-flex align-items-center g-0-5">
+        ${
+          product.isSoldOut
+            ? `<span class="price display_h3">매진</span>`
+            : `
+          <span class="price display_h3">${Number(product.salePrice).toLocaleString()}원</span>
+          <span class="discount_rate body_xl">${product.discountRate}%</span>
+          `
+        }
+        </div>
+      </div>
+    </article>
+  `;
+  productListEl.appendChild(itemEl);
+}
+function renderProducts(firstIndex = 0, lastIndex = firstIndex + 11) {
+  productListEl.innerHTML = "";
+  for (let i = firstIndex; i <= lastIndex; i++) {
+    renderProductCard(filteredProducts[i]);
+  }
 }
 
 // ==========================================
